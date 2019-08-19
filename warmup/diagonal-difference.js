f88c30905abed46143e1c5b5ae40df3d79f301d2
Bohdan Shulha <bohdan@shulha.pp.ua>
2019-08-19 16:16:28 +0300

    [JavaScript] Diagonal Difference

---
+++ warmup/diagonal-difference.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+function diagonalDifference(arr) {
+    const [straight, reversed] = arr.reduce(([s, r], row, i) => (
+        [s + row[i], r + row[row.length - i - 1]]
+    ), [0, 0]);
+
+    return Math.abs(straight - reversed);
+}
+
+// Standard boilerplate
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
