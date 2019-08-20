ea9f41a90b7eaee5667148351d1bae68298f908a
Bohdan Shulha <bohdan@shulha.pp.ua>
2019-08-20 12:15:18 +0300

    [JavaScript] Birthday Cake Candles

---
+++ warmup/birthday-cake-candles.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+function birthdayCakeCandles(arr) {
+    const maxN = Math.max.apply(null, arr);
+
+    return arr.filter(n => n === maxN).length;
+}
+
+// Standard boilerplate
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = birthdayCakeCandles(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
